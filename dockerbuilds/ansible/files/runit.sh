4c6514f239f59784ed2846e3dfed98716956c43a
testuser <testuser@test.com>
2017-10-11 21:25:25 -0700

    almost working

--- dockerbuilds/ansible/files/runit.sh
+++ dockerbuilds/ansible/files/runit.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
 
-ansible-playbook /etc/ansible/main.yml
+
+if [[ $1 == "on" ]]; then
+    echo "Bringing up cluster"
+    ansible-playbook -e cluster='up' /etc/ansible/main.yml
+else
+    echo "Tearing cluster down"
+    ansible-playbook -e cluster='down' /etc/ansible/main.yml
+fi
+
